e5168858f309b3f71850fb4f6bced7c1a6e289b6
shakifk1 <shakifk1@gmail.com>
2023-01-27 18:33:06 +0530

    some style

--- src/Component/Student.jsx
+++ src/Component/Student.jsx
@@ -19,27 +19,30 @@ const Student = () => {
   const PageChange = () => {
     setTrueFalse(!TrueFalse)
   }
-  
-  
+
+
   return (
     <div>
       <div>
-        <form>
-          { TrueFalse ?
+        <form className='form-rend'>
+          {TrueFalse ?
             <div>
-              <div className='form-div'><label htmlFor="name">Name:</label> <br />
-                <input name="Name1" value={Data.Name1} onChange={ChangeHandel} type="text" /><br /></div>
+              <div className='form-flex'>
+                <div className='form-div'><label htmlFor="name">Name:</label> <br />
+                  <input className='input-box' name="Name1" value={Data.Name1} onChange={ChangeHandel} type="text" /><br /></div>
 
-              <div className='form-div'><label htmlFor="age">Age</label><br />
-                <input name="Age" value={Data.Age} onChange={ChangeHandel} type="number" /><br /></div>
+                <div className='form-div'><label htmlFor="age">Age</label><br />
+                  <input className='input-box' name="Age" value={Data.Age} onChange={ChangeHandel} type="number" /><br /></div>
 
-              <div className='form-div'><label htmlFor="course">Course</label><br />
-                <input name="Course" value={Data.Course} onChange={ChangeHandel} type="text" /><br /></div>
+                <div className='form-div'><label htmlFor="course">Course</label><br />
+                  <input className='input-box' name="Course" value={Data.Course} onChange={ChangeHandel} type="text" /><br /></div>
 
-              <div className='form-div'><label htmlFor="batch">Batch</label><br />
-                <input name="Batch" value={Data.Batch} onChange={ChangeHandel} type="text" /></div>
-              <button onClick={PageChange}>Cancel</button>
-              <button type='button' onClick={ClickFunc}>Click Here</button>
+                <div className='form-div'><label htmlFor="batch">Batch</label><br />
+                  <input className='input-box' name="Batch" value={Data.Batch} onChange={ChangeHandel} type="text" /></div>
+
+              </div>
+              <button className='btn-form' onClick={PageChange}>Cancel</button>
+              <button className='btn-form' type='button' onClick={ClickFunc}>Submit</button>
             </div>
             :
             <div>
@@ -54,7 +57,7 @@ const Student = () => {
               </div>
 
               {
-                arrData.map((item,index) => {
+                arrData.map((item, index) => {
                   return (
                     <div key={index} className='flex1'>
                       <div className='box1'>{item.Name1}</div>
@@ -67,9 +70,9 @@ const Student = () => {
                 })
               }
             </div>
-    }
-    </form>
-    </div>
+          }
+        </form>
+      </div>
 
     </div>
   )
